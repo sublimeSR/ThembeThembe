5bc85ceb477e7bbddaf5166b1156137da7871618
sublimeSR <raut.samarth@gmail.com>
2018-10-14 14:12:55 +0530

    moved .gitignore and removed Deug op etc folders

--- Debug/src/Config.d
+++
@@ -1,4 +0,0 @@
-src/Config.d: ../src/Config.cpp \
- /home/sraut/workspace/eclipse/ThembeThembe/inc/Config.hpp
-
-/home/sraut/workspace/eclipse/ThembeThembe/inc/Config.hpp:
--- Debug/src/DueRecord.d
+++
@@ -1,7 +0,0 @@
-src/DueRecord.d: ../src/DueRecord.cpp \
- /home/sraut/workspace/eclipse/ThembeThembe/inc/DueRecord.hpp \
- /home/sraut/workspace/eclipse/ThembeThembe/inc/Record.hpp
-
-/home/sraut/workspace/eclipse/ThembeThembe/inc/DueRecord.hpp:
-
-/home/sraut/workspace/eclipse/ThembeThembe/inc/Record.hpp:
